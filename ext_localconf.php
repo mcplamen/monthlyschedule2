276167b78e291b75f3dd978e3dcf501ecaf18736
mcplamen <30468873+mcplamen@users.noreply.github.com>
2025-12-23 11:25:02 +0200

    2312

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,38 +1,41 @@
 <?php
 defined('TYPO3') || die();
 
-(static function() {
-    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-        'Monthlyschedule',
-        'Monthlyschedule',
-        [
-            \Mcplamen\Monthlyschedule\Controller\MymonthController::class => 'index, list, show',
-            \Mcplamen\Monthlyschedule\Controller\MydayController::class => 'index, list, show'
-        ],
-        // non-cacheable actions
-        [
-            \Mcplamen\Monthlyschedule\Controller\MymonthController::class => 'new, create, edit, update, delete',
-            \Mcplamen\Monthlyschedule\Controller\MydayController::class => 'new, create, edit, update, delete, select'
-        ]
-    );
+// DEBUG - This will create a file if ext_localconf.php is loaded
+file_put_contents('/tmp/ext_localconf_loaded.txt', date('Y-m-d H:i:s') . " - File loaded\n", FILE_APPEND);
 
-    // wizards
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
-        'mod {
-            wizards.newContentElement.wizardItems.plugins {
-                elements {
-                    monthlyschedule {
-                        iconIdentifier = monthlyschedule-plugin-monthlyschedule
-                        title = LLL:EXT:monthlyschedule/Resources/Private/Language/locallang_db.xlf:tx_monthlyschedule_monthlyschedule.name
-                        description = LLL:EXT:monthlyschedule/Resources/Private/Language/locallang_db.xlf:tx_monthlyschedule_monthlyschedule.description
-                        tt_content_defValues {
-                            CType = list
-                            list_type = monthlyschedule_monthlyschedule
-                        }
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+    'Monthlyschedule',  // FIXED: Changed from 'Mcplamen.Monthlyschedule'
+    'Monthlyschedule',
+    [
+        \Mcplamen\Monthlyschedule\Controller\MymonthController::class => 'index, list, show',
+        \Mcplamen\Monthlyschedule\Controller\MydayController::class => 'index, list, show'
+    ],
+    [
+        \Mcplamen\Monthlyschedule\Controller\MymonthController::class => 'new, create, edit, update, delete',
+        \Mcplamen\Monthlyschedule\Controller\MydayController::class => 'new, create, edit, update, delete, select'
+    ]
+);
+
+// DEBUG - Check if configurePlugin succeeded
+file_put_contents('/tmp/ext_localconf_loaded.txt', "configurePlugin called\n", FILE_APPEND);
+file_put_contents('/tmp/ext_localconf_loaded.txt', print_r($GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['extbase']['extensions']['Monthlyschedule'] ?? 'NOT SET', true) . "\n", FILE_APPEND);
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
+    'mod {
+        wizards.newContentElement.wizardItems.plugins {
+            elements {
+                monthlyschedule {
+                    iconIdentifier = monthlyschedule-plugin-monthlyschedule
+                    title = LLL:EXT:monthlyschedule/Resources/Private/Language/locallang_db.xlf:tx_monthlyschedule_monthlyschedule.name
+                    description = LLL:EXT:monthlyschedule/Resources/Private/Language/locallang_db.xlf:tx_monthlyschedule_monthlyschedule.description
+                    tt_content_defValues {
+                        CType = list
+                        list_type = monthlyschedule_monthlyschedule
                     }
                 }
-                show = *
             }
-       }'
-    );
-})();
+            show = *
+        }
+    }'
+);
--- ext_tables.php
+++ ext_tables.php
@@ -1,10 +1,14 @@
 <?php
 defined('TYPO3') || die();
 
-(static function() {
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_monthlyschedule_domain_model_mymonth', 'EXT:monthlyschedule/Resources/Private/Language/locallang_csh_tx_monthlyschedule_domain_model_mymonth.xlf');
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_monthlyschedule_domain_model_mymonth');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr(
+    'tx_monthlyschedule_domain_model_mymonth', 
+    'EXT:monthlyschedule/Resources/Private/Language/locallang_csh_tx_monthlyschedule_domain_model_mymonth.xlf'
+);
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_monthlyschedule_domain_model_mymonth');
 
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_monthlyschedule_domain_model_myday', 'EXT:monthlyschedule/Resources/Private/Language/locallang_csh_tx_monthlyschedule_domain_model_myday.xlf');
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_monthlyschedule_domain_model_myday');
-})();
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr(
+    'tx_monthlyschedule_domain_model_myday', 
+    'EXT:monthlyschedule/Resources/Private/Language/locallang_csh_tx_monthlyschedule_domain_model_myday.xlf'
+);
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_monthlyschedule_domain_model_myday');
